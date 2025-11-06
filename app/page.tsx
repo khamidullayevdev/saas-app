a3386cdf49f8b2b679a5fd0aea6e9613bb3f13e1
CodeeBy <khamidullayevammoriddin@gmail.com>
2025-11-06 19:51:57 +0500

    Home Page ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,49 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>U didn't see it</h1>
-      <Button>
-        Let's start it mfs
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+      
+      <section className='home-section'>
+        <CompanionCard 
+          id='123'
+          name='Neura the Brainy Explorer'
+          topic='Neural Network of the Brain'
+          subject='Science'
+          duration={45}
+          color='#E5D0FF'
+        />
+        <CompanionCard 
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives & Integrals'
+          subject='Maths'
+          duration={30}
+          color='#FFDA6E'
+        />
+        <CompanionCard 
+          id='136'
+          name='Verba the Vocabulary Builder'
+          topic='English Literature'
+          subject='Language'
+          duration={30}
+          color='#BDE7FF'
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList 
+          title='Recently completed lessons'
+          companions={recentSessions}
+          classNames='w-2/3 max-lg:w-full'
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
