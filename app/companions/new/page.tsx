634245fb863594bbaabd14998e24a2b58be68908
CodeeBy <khamidullayevammoriddin@gmail.com>
2025-11-07 11:20:26 +0500

    Auth, Payment integration with clerk

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,14 @@
+import CompanionForm from "@/components/CompanionForm"
+
 const CompanionNew = () => {
   return (
-    <div>CompanionNew</div>
+    <main className="lg:w-1/3 md:w-2/3 items-center justify-center">
+      <article className="w-full flex flex-col gap-4">
+        <h1>Companion Builder</h1>
+
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
